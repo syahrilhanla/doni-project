d85b5cf090d412d5b6a11f10021dd66130795619
aaldyy <aldirenaldialamin@Gmail.com>
2023-01-11 15:06:13 +0800

    Create UI Login Page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,67 +3,73 @@ import React from "react";
 const Dashboard = () => {
 	return (
 		<div>
-			<section className="h-screen">
-				<div className="px-6 h-full text-gray-800">
-					<div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
-										<div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-							<form>
-								<div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-									<p className="text-center font-semibold mx-4 mb-0">Login</p>
+		<section className="h-screen">
+			<div className="px-6 h-full text-gray-800">
+			<div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+			<div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-2 md:mb-0">
+			<form>
+			<div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+									<p className="text-center text-[#5E548E] font-semibold mx-4 mb-0 text-4xl">Login</p>
 								</div>
-
-								<div className="mb-6">
-									<input
-										type="text"
-										className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-										id="exampleFormControlInput2"
-										placeholder="Username"
-									/>
-								</div>
-
-								<div className="mb-6">
-									<input
-										type="password"
-										className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-										id="exampleFormControlInput2"
-										placeholder="Password"
-									/>
-								</div>
-
-								<div className="flex justify-between items-center mb-6">
-									<div className="form-group form-check">
-										<input
-											type="checkbox"
-											className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-											id="exampleCheck2"
-										/>
-										<label className="form-check-label inline-block text-gray-800">
-											Remember me
-										</label>
-									</div>
-							</div>
-
-								<div className="text-center lg:text-left">
-									<button
-										type="button"
-										className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-									>
-										Login
-									</button>
-									<p className="text-sm font-semibold mt-2 pt-1 mb-0">
-										Don't have an account?
+				<div className="mb-6">
+					<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+						Username
+					</label>
+					<input
+						type="email"
+						id="email"
+						className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						placeholder="ex: 1710131310001"
+						required
+					/>
+				</div>
+				<div className="mb-6">
+					<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+						Password
+					</label>
+					<input
+						type="password"
+						id="password"
+						className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						required
+					/>
+				</div>
+				<div className="flex items-start mb-6">
+					<div className="flex items-center h-5">
+						<input
+							type="checkbox"
+							id="remember"
+							value=""
+							className="w-4 h-4 border accent-[#9F86C0]  border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-[#BE95C4] dark:bg-[#BE95C4] dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+							required
+						/>
+					</div>
+					<label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+						Remember me
+					</label>
+				</div>
+				<button
+					type="submit"
+					className="text-white bg-[#5E548E] hover:bg-[#231942] focus:ring-4 focus:outline-none focus:ring-navy-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-[#E0B1CB] dark:hover:bg-[#BE95C4] dark:focus:ring-blue-800"
+				>
+					LOGIN
+				</button>
+				<div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"/>
+				<div className="text-center">
+				<p className="text-sm font-semibold mt-2 pt-1 mb-0">
+										Don't have an account? 
 										<a
 											href="#!"
-											className="text-blue-800 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+											className="text-[#BE95C4] hover:text-[#5E548E] focus:text-red-700 transition duration-200 ease-in-out"
 										>
 											Register
 										</a>
 									</p>
-								</div>
-							</form>
-						</div>
-					</div>
 				</div>
+			</form>
+			</div>
+			</div>
+			</div>
 			</section>
 		</div>
 	);
