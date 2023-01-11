ba13551b2020be475706cf8c602bdfaa0dd60784
diimasmuhammad <diimasmuhammad@gmail.com>
2023-01-11 23:55:42 +0800

    Add register Page

---
+++ pages/register.tsx
@@ -0,0 +1,160 @@
+import React from "react";
+
+export default function Register() {
+  return (
+    <div>
+      <section className="h-screen overflow-auto">
+        <div className="px-6 h-max dark:text-white text-gray-800 dark:bg-gray-800 ">
+          <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+            <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-2 md:mb-0">
+              <form>
+                <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+                  <p className="text-center text-patternOne font-semibold mx-4 mb-0 text-4xl dark:text-patternFive">
+                    Daftar
+                  </p>
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Nama
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Nama Pengguna
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="ex1710131210007"
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Nomer Telepon / HP
+                  </label>
+                  <input
+                    type="text"
+                    id="phoneNumber"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Kata Sandi
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Konfirmasi Kata Sandi
+                  </label>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Dosen Pembimbing 1
+                  </label>
+                  <select
+                    name=""
+                    id="listProfOne"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  >
+                    <option value="defaultProf">
+                      Pilih Dosen Pembimbing 1
+                    </option>
+                    <option value="profOne">Dosen 1</option>
+                    <option value="profTwo">Dosen 2</option>
+                  </select>
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Dosen Pembimbing 1
+                  </label>
+                  <select
+                    name=""
+                    id="listProfTwo"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  >
+                    <option value="defaultProf">
+                      Pilih Dosen Pembimbing 2
+                    </option>
+                    <option value="profOne">Dosen 1</option>
+                    <option value="profTwo">Dosen 2</option>
+                  </select>
+                </div>
+
+                <div className="flex items-start mb-6">
+                  <div className="flex items-center h-5">
+                    <input
+                      type="checkbox"
+                      id="remember"
+                      value=""
+                      className="w-4 h-4 border accent-patternTwo  border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-patternThree dark:bg-patternThree dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                      required
+                    />
+                  </div>
+                  <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                    Ingat Aku
+                  </label>
+                </div>
+                <button
+                  type="submit"
+                  className="text-white bg-patternOne hover:bg-patternFour focus:ring-4 focus:outline-none focus:ring-navy-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:text-patternFour dark:bg-patternFive dark:hover:bg-patternThree dark:focus:ring-blue-800"
+                >
+                  DAFTAR
+                </button>
+                <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5" />
+                <div className="text-center">
+                  <p className="text-sm font-semibold mt-2 pt-1 mb-4">
+                    Sudah punya akun ?{" "}
+                    <a
+                      href="#!"
+                      className=" text-patternThree hover:text-patternOne focus:text-red-700 transition duration-200 ease-in-out dark:text-patternFive"
+                    >
+                      Masuk
+                    </a>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
