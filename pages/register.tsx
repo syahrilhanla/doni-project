9d0868efb2ef9a6d8ce1bddd7054e5b47f0f3c6b
diimasmuhammad <diimasmuhammad@gmail.com>
2023-01-13 21:27:16 +0800

    Bug fix minLength

--- pages/register.tsx
+++ pages/register.tsx
@@ -107,10 +107,9 @@ export default function Register() {
                     type="password"
                     {...register("password", {
                       required: "Password diperlukan",
-                      min: {
+                      minLength: {
                         value: 6,
-                        message:
-                          "Password harus harus memiliki minimal 6 karakter",
+                        message: "Password harus memiliki minimal 6 karakter",
                       },
                     })}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
