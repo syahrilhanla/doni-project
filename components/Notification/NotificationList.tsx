05d1e86d1b8245efa9682eb27b5ca87fc285d680
syahrilhanla <syahrilhanla5@gmail.com>
2023-01-16 23:13:01 +0700

    adjustment on notification list style

--- components/Notification/NotificationList.tsx
+++ components/Notification/NotificationList.tsx
@@ -12,24 +12,25 @@ interface Props {
 const NotificationList = ({ notificationData }: Props) => {
   return (
     <div
-      className="absolute w-64 max-h-[268px] py-2 px-5
-       gap-4 overflow-auto rounded-2xl bg-[#F0EBF8] 
+      className="absolute w-64 max-h-[268px]
+        gap-4 overflow-auto rounded-xl bg-[#F0EBF8] 
         top-[68px] right-20 z-50"
     >
       {notificationData.map((data: any) => (
         <div
           key={data.id}
-          className="flex flex-col text-[#683ab7d5] text-justify
-            w-full gap-1.5 py-1.5"
+          className="flex flex-col text-[#683ab7d5] text-justify border-b
+            w-full gap-1.5 py-1.5 cursor-pointer hover:bg-[#683ab715]"
         >
-          <span className="flex items-center gap-1">
-            {data.isRead && (
-              <span className="p-1 h-1 w-1 rounded-full bg-red-600" />
-            )}
-            <p>{data.title}</p>
-          </span>
-          <p>{data.text}</p>
-          <hr />
+          <div className="w-full px-3 py-1">
+            <span className="flex items-center gap-1">
+              {data.isRead && (
+                <span className="p-1 h-1 w-1 rounded-full bg-red-600" />
+              )}
+              <p>{data.title}</p>
+            </span>
+            <p>{data.text}</p>
+          </div>
         </div>
       ))}
     </div>)
