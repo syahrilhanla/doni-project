d94ed1883abe53e06cc7284fec37ba2be2ec50d8
syahrilhanla <syahrilhanla5@gmail.com>
2023-01-16 23:53:26 +0700

    fix git ignore case

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import Navbar from "../components/Navbar/Navbar";
 
 interface formInput {
   name: String;
