191372155b00cc0766c4c667be5db01a1c4896ee
diimasmuhammad <diimasmuhammad@gmail.com>
2023-01-25 08:27:14 +0800

    - Add List Dosen - Add List Mahasiswa - Add Request List - Add Modal in Approval, Progress, Seminar List, Sidang List Page - Add Sidebar Admin - Add Layout Admin

---
+++ components/Layout/LayoutAdmin.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import SidebarAdmin from "../Sidebar/SidebarAdmin";
+import FilterSection from "./FilterSection";
+
+export default function LayoutAdmin(props: any) {
+  return (
+    <div className="text-center flex flex-col min-h-screen relative">
+      <Navbar />
+      <div className="w-full min-h-[90vh] flex flex-row">
+        <span className="hidden sm:block">
+          <SidebarAdmin />
+        </span>
+        <div className="w-full flex flex-col items-center p-4">
+          <FilterSection />
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
