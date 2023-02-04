8cfe31de8ae8472cf703420b76e6047da15fc637
syahrilhanla <syahrilhanla5@gmail.com>
2023-02-04 09:17:25 +0700

    debug some ajukan modal shit

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,41 +5,49 @@ import { RiCloseLine } from "react-icons/ri";
 import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
 import { useAuth } from "../components/Context/AuthContext";
 import {
-  collection,
   doc,
-  onSnapshot,
-  orderBy,
-  query,
   updateDoc,
 } from "firebase/firestore";
 import { db } from "../components/Store/firebase";
 
-interface DataType {
-  title: string;
-}
 const Dashboard = () => {
-  const [title, setTitle] = useState<any>();
-  const [ajukan, setAjukan] = useState<any>(false);
+  const [title, setTitle] = useState<string>();
+  const [isOpenModal, setIsOpenModal] = useState<any>(false);
   const { user } = useAuth();
-  console.log(user);
 
-  const getTitle = (title: DataType) => {
+  const getTitle = (title: string) => {
     // setTitle(title);
-    setAjukan(!ajukan);
+    // setAjukan(!ajukan);
   };
-  const updateTitle = (title: DataType) => {
-    let fieldEdit = doc(db, "studentsList");
-    updateDoc(fieldEdit, {
-      title: title,
-    })
-      .then(() => {
-        alert("Judul berhasil ditambahkan");
-        setAjukan(!ajukan);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+
+  // const updateTitle = (title: string) => {
+  //   let fieldEdit = doc(db, "studentsList");
+  //   updateDoc(fieldEdit, {
+  //     title: title,
+  //   })
+  //     .then(() => {
+  //       alert("Judul berhasil ditambahkan");
+  //       setAjukan(!ajukan);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
+  useEffect(() => {
+    // if (user.title) setTitle(user.title[0].tittleText)
+  }, [user]);
+
+  const handleChange = (value: string) => {
+    // setTitle(value);
+    console.log(value);
+    setTitle(value);
+  }
+
+  const updateTitle = (titleValue: any) => {
+    console.log(titleValue);
+    setIsOpenModal(false)
+  }
 
   return (
     <Layout>
@@ -52,35 +60,40 @@ const Dashboard = () => {
             <div className="font-black ">{user.username}</div>
           </div>
           <div className="flex justify-center xxs:w-full items-center sm:max-md:w-full  md:max-lg:w-full  md:max-lg:mt-3 w-3/5 h-24 bg-[#f2e8f24f] text-[#683ab7d5] rounded-lg shadow-md">
-            {title ? (
+            {
+              title &&
               <>
-                <div className="flex justify-items-center font-sans italic">
-                  <p>{user.title}</p>
+                <div className="w-full text-left px-4">
+                  {user.title[0].tittleText !== "" ? user.title[0].tittleText : title}
                 </div>
               </>
-            ) : (
+            }
+
+            {!title && (
               <>
                 <div className="flex justify-between w-full mx-7">
                   <div className="text-lg items-center text-gray-500  font-sans">
-                    {user.title}
+                    Belum ada judul
                   </div>
                   <div>
                     <button
-                      className="text-white  bg-patternTwo hover:text-gray-900 hover:bg-[#c9c2d2] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
-                      onClick={() => getTitle(user.title)}
+                      className="text-white  bg-patternTwo hover:text-gray-900 hover:bg-[#c9c2d2] 
+                          font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                      onClick={() => setIsOpenModal(true)}
                     >
                       Ajukan Judul
                     </button>
                   </div>
 
-                  {ajukan && (
+
+                  {isOpenModal && (
                     <>
                       <div className=" flex justify-center items-center fixed top-0 left-0 right-0 z-50  p-4 overflow-x-hidden overflow-y-auto w-screen h-screen mx-auto ">
                         <div className="bg-gray-700 opacity-30 h-screen w-screen -z-50 absolute top-0 left-0 right-0" />
                         <div className="gap-4 relative  w-3/5 h-full  flex justify-center items-center">
                           <div className="relative bg-white border-purple-600 rounded-2xl shadow w-3/5 xxs:max-md:w-full md:max-lg:w-full min-h-fit ">
                             <button
-                              onClick={() => setAjukan(!ajukan)}
+                              onClick={() => setIsOpenModal(false)}
                               type="button"
                               className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-red-500 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
                             >
@@ -94,13 +107,13 @@ const Dashboard = () => {
                                 <textarea
                                   placeholder="Masukkan Judul Skripsi"
                                   value={title}
-                                  onChange={(e) => setTitle(e.target.value)}
+                                  onChange={(e) => handleChange(e.target.value)}
                                   className="min-h-[100px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-none block w-full p-2.5"
                                   required
                                 />
                               </form>
                               <button
-                                onClick={() => updateTitle}
+                                onClick={() => updateTitle(title)}
                                 type="button"
                                 className=" text-white bg-patternTwo focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 min-h-[50px] mt-3  hover:text-white focus:z-10"
                               >
@@ -115,6 +128,7 @@ const Dashboard = () => {
                 </div>
               </>
             )}
+
           </div>
         </div>
         {/* dospem */}
