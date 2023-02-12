0bc96a259d4d262b4079f55f72c65a6de5062623
aaldyy <aldirenaldialamin@Gmail.com>
2023-02-12 19:36:27 +0800

    Updating 404 design

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,12 +1,12 @@
 
-import { onSnapshot } from "firebase/firestore";
 import Router from "next/router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useAuth } from "../Context/AuthContext";
 import Navbar from "../Navbar/Navbar";
 import ProtectedRoute from "../ProtectedRoute";
 import Sidebar from "../Sidebar/Sidebar";
-import { db } from "../Store/firebase";
+
+import {TiWarningOutline} from "react-icons/ti"
 
 type DashboardLayoutProps = {
   children: React.ReactNode;
@@ -21,9 +21,6 @@ const Layout = ({ children }: DashboardLayoutProps) => {
     Router.push("/approval")
   }
   const [approve, setApprove] = useState(user.statusApprove)
-  // useEffect(()=>{
-  //   onSnapshot(doc(db,"studentsList"))
-  // },[])
   return (
     <ProtectedRoute>
 
@@ -41,15 +38,16 @@ const Layout = ({ children }: DashboardLayoutProps) => {
           :
           <>
             <Navbar />
-            <div className="w-full min-h-[90vh] flex flex-row">
+            <div className="w-full min-h-[100vh] flex flex-row">
               <span className="hidden sm:block">
                 <Sidebar />
               </span>
               <div className="w-full h-full">
-                <div className="h-screen">
-                  <div className="flex xl:justify-center lg:justify-center justify-center items-center flex-wrap h-full g-6">
-                    <div className="xl:ml-20 xl:w-5/12 lg:w-8/12 md:w-8/12 mb-2 md:mb-0 border-4  h-80 border-[#cccbccf3] rounded-2xl bg-white">
-                      <p className="grid content-center justify-items-center text-5xl text-center text-[#a68ea8f3]">
+                <div className="h-screen bg-[#a5a4a4a9]">
+                  <div className="flex xl:justify-center lg:justify-center justify-center items-center  h-full g-6">
+                    <div className="flex flex-col items-center xl:ml-20 xl:w-5/12 lg:w-8/12 md:w-8/12  h-80 border-[#cccbccf3] rounded-xl bg-white">
+                      <div className="text-9xl text-[#fd8888] mt-5 "><TiWarningOutline /></div>
+                      <p className="sm:text-2xl sm:flex flex-col xl:text-5xl text-center text-[#9e9d9ef3]">
                         Menunggu ...
                         Konfirmasi Admin
                       </p>
