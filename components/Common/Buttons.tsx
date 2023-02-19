6dcd98aecee6b3da12e620b57c39524599da352c
syahrilhanla <syahrilhanla5@gmail.com>
2023-02-19 08:14:17 +0700

    create button components

---
+++ components/Common/Buttons.tsx
@@ -0,0 +1,27 @@
+import { RiCloseLine } from "react-icons/ri";
+
+export const CloseButton = ({ handleClick }: Props) => {
+  return (
+    <button
+      onClick={handleClick}
+      type="button"
+      className="absolute top-3 right-2.5 hover:bg-[#ef4e4e] hover:text-[#f3fef3] p-2 rounded-md duration-200"
+    >
+      <RiCloseLine />
+    </button>)
+}
+
+export const SendButton = ({ handleClick }: Props) => {
+  return (
+    <button
+      onClick={handleClick}
+      className="purple-button"
+    >
+      Kirim
+    </button>
+  )
+}
+
+interface Props {
+  handleClick: () => void
+}
