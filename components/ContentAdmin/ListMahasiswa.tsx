4e3a74f1e44f0c9588924509996e47f0c72bab4b
syahrilhanla <syahrilhanla5@gmail.com>
2023-02-19 10:57:10 +0700

    add error button style to button components and implement it to ListMahasiswa page

--- components/ContentAdmin/ListMahasiswa.tsx
+++ components/ContentAdmin/ListMahasiswa.tsx
@@ -15,7 +15,7 @@ import {
 } from "firebase/firestore";
 import { db } from "../Store/firebase";
 import { async } from "@firebase/util";
-import { CloseButton, SendButton } from "../Common/Buttons";
+import { CloseButton, ErrorButton, SendButton } from "../Common/Buttons";
 import Dropdown from "../Common/Dropdown";
 
 interface dataTable {
@@ -294,31 +294,15 @@ export default function ListMahasiswa() {
             <div className="bg-gray-700 opacity-30 h-screen w-screen -z-50 absolute top-0 left-0 right-0" />
             <div className="gap-4 relative  w-3/5 h-full  flex justify-center items-center">
               <div className="relative bg-white border-purple-600 rounded-2xl shadow w-3/5 xxs:max-md:w-full md:max-lg:w-full min-h-fit ">
-                <button
-                  onClick={() => setHapus(!hapus)}
-                  type="button"
-                  className="absolute top-3 right-2.5 bg-red-600 hover:text-red-600 hover:bg-white text-white bg-transparent hover:ring-red-600 ring-1 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-                >
-                  <RiCloseLine />
-                </button>
+                <CloseButton handleClick={() => setHapus(false)} />
 
-                <div className="p-4 flex flex-col gap-2">
+                <div className="p-4 flex flex-col gap-2 mt-4">
                   <p className="block text-xl mt-6 font-medium text-gray-900 ">
                     Apakah anda ingin menghapus data mahasiswa ini?
                   </p>
                   <div className="p-4 flex gap-2 justify-end items-end">
-                    <button
-                      type="button"
-                      className=" text-white bg-green-500 ring-2  rounded-lg  text-sm font-medium px-5 min-h-[50px] mt-3  hover:text-green-500 hover:ring-green-500 hover:bg-white focus:z-10"
-                    >
-                      Iya
-                    </button>
-                    <button
-                      type="button"
-                      className=" text-white bg-red-500 ring-2  rounded-lg  text-sm font-medium px-5 min-h-[50px] mt-3  hover:text-red-500 hover:ring-red-500 hover:bg-white focus:z-10"
-                    >
-                      Tidak
-                    </button>
+                    <ErrorButton handleClick={() => setHapus(false)} buttonText="Hapus" />
+                    <SendButton handleClick={() => setHapus(false)} buttonText="Batal" />
                   </div>
                 </div>
               </div>
