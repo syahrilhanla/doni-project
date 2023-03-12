27b6b306903dabddc763657a1a6c227893b978d0
diimasmuhammad <diimasmuhammad@gmail.com>
2023-03-12 20:34:07 +0800

    - Add modal and logic function to add new prof - Add component AddProf - Change email placeholder

--- pages/register.tsx
+++ pages/register.tsx
@@ -112,7 +112,7 @@ export default function Register() {
                 type="email"
                 {...register("email")}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                placeholder="email@gmail.com"
+                placeholder="email@mhs.ulm.ac.id"
               />
               <p className="text-red-500">{errors.email?.message}</p>
             </div>
