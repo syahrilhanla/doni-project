fa4ae08d62829e9c50a925a5660c10b64c6eab4d
syahrilhanla <syahrilhanla5@gmail.com>
2023-03-23 14:26:28 +0700

    removed unused imports on Navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,24 +1,11 @@
-import React, { useCallback, useEffect, useState } from "react";
-import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
 
 import { RiLogoutBoxRLine } from "react-icons/ri";
 import { TfiBell } from "react-icons/tfi";
-import NotificationList, {
-  NotificationData,
-} from "../Notification/NotificationList";
+import NotificationList from "../Notification/NotificationList";
 import { useAuth } from "../Context/AuthContext";
 import { useRouter } from "next/router";
-import {
-  arrayUnion,
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  query,
-  updateDoc,
-  where,
-} from "firebase/firestore";
-import { User } from "firebase/auth";
+import { doc, getDoc, } from "firebase/firestore";
 import { db } from "../Store/firebase";
 
 const Navbar = () => {
@@ -27,7 +14,7 @@ const Navbar = () => {
   const router = useRouter();
   const [notification, setNotification] = useState([]);
   const [notificationdosen, setNotificationDosen] = useState<any>([]);
-   const [student, setStudent] = useState<any>([]);
+  const [student, setStudent] = useState<any>([]);
 
   const handleLogout = async () => {
     try {
@@ -37,6 +24,7 @@ const Navbar = () => {
       console.log(error);
     }
   };
+
   const getNotifDosen = useCallback(async () => {
     try {
       const profRef = doc(db, "professorList", user.uid);
@@ -46,6 +34,7 @@ const Navbar = () => {
       console.log(error);
     }
   }, []);
+
   const getNotifMahasiswa = useCallback(async () => {
     try {
       const studentsRef = doc(db, "studentsList", user.uid);
@@ -58,51 +47,11 @@ const Navbar = () => {
 
   useEffect(() => {
     if (user.role === "mhs") {
-       getNotifMahasiswa();
+      getNotifMahasiswa();
     } else if (user.role === "dosen") {
       getNotifDosen();
     }
   }, [user]);
-
-  const notificationData: NotificationData[] = [
-    {
-      id: 1,
-      title: "title 1",
-      text: "Text 1",
-      isRead: true,
-    },
-    {
-      id: 2,
-      title: "title 2",
-      text: "text 2",
-      isRead: false,
-    },
-    {
-      id: 11,
-      title: "title 1",
-      text: "Text 1",
-      isRead: true,
-    },
-    {
-      id: 22,
-      title: "title 2",
-      text: "text 2",
-      isRead: false,
-    },
-    {
-      id: 115,
-      title: "title 1",
-      text: "Text 1",
-      isRead: true,
-    },
-    {
-      id: 22,
-      title: "title 2",
-      text: "text 2",
-      isRead: false,
-    },
-  ];
-
 
   return (
     <div className="flex flex-col top-12">
